2db23dbb8abf068401495b176350fc80c4810c10
x404 <kronos2003@gmail.com>
2018-04-05 23:13:25 +0300

    Добавлен скрипт прокрутк страницы вверх

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -19,42 +19,49 @@ $(document).ready(function(){
 		autoplaySpeed: 2000
 	});
 
-	// карусель
-	$('#foo1').owlCarousel({
-		loop:false,
-		nav:true,
-		dots: false,
-		items:3,
-		startPosition : 2,
-		stagePadding : 250,
-		navText: ["", ""],
-		onInitialized: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		onChanged: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		responsive:{
-			0:{
-				items:1,
-				stagePadding: 20
-			},
-			900:{
-				items:2,
-				stagePadding: 0
-			},
-			992:{
-				items:1
-			},
-			1250:{
-				items:2
-			},
-			1550:{
-				items:3
-			}
-		}
-	});
+	// // карусель
+	// $('#foo1').owlCarousel({
+	// 	loop:false,
+	// 	nav:true,
+	// 	dots: false,
+	// 	items:3,
+	// 	startPosition : 2,
+	// 	stagePadding : 250,
+	// 	navText: ["", ""],
+	// 	onInitialized: function (event) {
+	// 		refreshFirstLastVisible(event);
+	// 	},
+	// 	onChanged: function (event) {
+	// 		refreshFirstLastVisible(event);
+	// 	},
+	// 	responsive:{
+	// 		0:{
+	// 			items:1,
+	// 			stagePadding: 20
+	// 		},
+	// 		900:{
+	// 			items:2,
+	// 			stagePadding: 0
+	// 		},
+	// 		992:{
+	// 			items:1
+	// 		},
+	// 		1250:{
+	// 			items:2
+	// 		},
+	// 		1550:{
+	// 			items:3
+	// 		}
+	// 	}
+	// });
 
+
+	$('#totop').click(function (){
+		$("body,html").animate({
+			scrollTop:0
+		}, 800);
+		return false;
+	});
 
 	// mobile-menu
 	$('#navbar').each(function(){
